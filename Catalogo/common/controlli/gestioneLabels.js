282d32d8b2585896ba1398271ba631a993ed7ee4
florenzi79 <francesco.lorenzi@gmail.com>
2015-08-31 13:21:36 +0200

    Controllo labels
    Snippet per inserire un controllo di modifica del testo delle labels

---
+++ Catalogo/common/controlli/gestioneLabels.js
@@ -0,0 +1,5 @@
+var mappaLabels = new java.util.LinkedHashMap();
+mappaLabels.put('it','Etichietta');
+items.get('idItem').setLabels(mappaLabels);
+fieldsets.get('idFieldset').setLabels(mappaLabels);
+modules.get('idModule').setLabels(mappaLabels);
