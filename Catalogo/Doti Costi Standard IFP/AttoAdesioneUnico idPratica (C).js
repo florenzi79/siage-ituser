286fda41d308f8d16a30343fc1e5ac6d9917448c
Marco Scotti <marco.scotti@astir.com>
2015-09-01 11:40:24 +0200

    Ripristino versione modificata da marco dopo conflitto con modifiche Fabiola. I campi AttoUnico_RappresentanteLegaleNome AttoUnico_RappresentanteLegaleCognome AttoUnico_RappresentanteLegaleCodiceFiscale sono copiati dai rispettivi campi Richiedente_   più sotto nel codice

--- Catalogo/Doti Costi Standard IFP/AttoAdesioneUnico idPratica (C).js
+++ Catalogo/Doti Costi Standard IFP/AttoAdesioneUnico idPratica (C).js
@@ -3,6 +3,8 @@
 // PARAMETRI da valorizzare correttamente usando il CATALOGO
 		var idStatiAttoUnicoPresentato = "'859506c362764ba1a70277d1345e7cee','8c8999ca9712409f81b1f5c39ef2f052'";
 		var nomeTemplate ="Doti IeFP DDF I anni"; // usato nella query per il check dell'atto unico
+//		var nomeTemplate ="TEST CATALOGO Dote IFP "; // temporaneo... per prova ********
+
 		var idFildsetDettagliAttoUnicoPresentato = 'e242e43908354a728b9c7286441a0245'; // viene nascosto o mostrato a secondo che è stato o non è stato presentato l'atto di adesione unico
 		var idFildsetFirmatario = '9286c54bf9b442bb831963add45108e1';
 		var idFildsetDichiarazioni = 'f14ac1d78de84e8c92e1b0bbd5235110';
@@ -15,21 +17,12 @@
 		values.put('Bando_Descrizione', descrizioneBando); // da definire cosa dovrà essere nel caso in cui si sta compilando l'atto unico
 		values.put('Richiedente_SedeLegaleDescrizione', descrizioneSedeLegale); // da definire cosa dovrà essere nel caso in cui si sta compilando l'atto unico
 
-        if ((!isEmpty('Partecipante_Cognome')) && (!isEmpty('Partecipante_Nome'))) {
-				values.put('title',values.get('Partecipante_Cognome')+' '+values.get('Partecipante_Nome'));
-		}
-		
-        var denominazione="";
-		if (!isEmpty('Richiedente_Denominazione')) {
-		    denominazione = values.get('Richiedente_Denominazione');
-		}
-		var titolo="";
-		if (!isEmpty('title')) {
-		    titolo = values.get('title') + ' ';
+		if (!isEmpty('Richiedente_Denominazione') && !isEmpty('title')) {
 				values.put('TitoloPratica',values.get('title') + ' ' + values.get('Richiedente_Denominazione'));
 		}
-		values.put('TitoloPratica',titolo + denominazione);
-		
+		if ((!isEmpty('Partecipante_Cognome')) && (!isEmpty('Partecipante_Nome'))) {
+				values.put('title',values.get('Partecipante_Cognome')+' '+values.get('Partecipante_Nome'));
+		}
 	//    items.get('title').setHidden(true);
 
 
@@ -63,12 +56,12 @@
 		}
 		if((mappaValoriSgProf.get('AA060') != null)&&(isEmpty('Richiedente_SedeLegaleIndirizzo')))
 			values.put( 'Richiedente_SedeLegaleIndirizzo', mappaValoriSgProf.get('AA060').toString() );
-		if( mappaValoriSgProf.get('AA030') != null && isEmpty('AttoUnico_RappresentanteLegaleNome'))
-			values.put( 'AttoUnico_RappresentanteLegaleNome', mappaValoriSgProf.get('AA030').toString() );
-		if( mappaValoriSgProf.get('AA029') != null && isEmpty('AttoUnico_RappresentanteLegaleCognome'))
-			values.put( 'AttoUnico_RappresentanteLegaleCognome', mappaValoriSgProf.get('AA029').toString() );
-		if( mappaValoriSgProf.get('AA142') != null && isEmpty('AttoUnico_RappresentanteLegaleCodiceFiscale') )
-			values.put( 'AttoUnico_RappresentanteLegaleCodiceFiscale', mappaValoriSgProf.get('AA142').toString() );
+		if( mappaValoriSgProf.get('AA030') != null && isEmpty('Richiedente_RappresentanteLegaleNome'))
+			values.put( 'Richiedente_RappresentanteLegaleNome', mappaValoriSgProf.get('AA030').toString() );
+		if( mappaValoriSgProf.get('AA029') != null && isEmpty('Richiedente_RappresentanteLegaleCognome'))
+			values.put( 'Richiedente_RappresentanteLegaleCognome', mappaValoriSgProf.get('AA029').toString() );
+		if( mappaValoriSgProf.get('AA142') != null && isEmpty('Richiedente_RappresentanteLegaleCodiceFiscale') )
+			values.put( 'Richiedente_RappresentanteLegaleCodiceFiscale', mappaValoriSgProf.get('AA142').toString() );
 	} // **** FINE LETTURA da SGPROF
     // Verifica Se atto unico è stato presentato
 
@@ -79,9 +72,6 @@
 	logger.info("XXXXX DOTE IFP: Numero Atti unici presentati per il CF "+CF_Operatore+": "+numAttiUniciPresentati);
 	if(numAttiUniciPresentati >0){
 		isAttoUnicoPresentato = true;
-		// MOSTRA AVVISO di atto unico ok e DATIPROCOCOLLAZIONE ATTO UNICO PRESENTATO
-		fieldsets.get(idFildsetDettagliAttoUnicoPresentato).setHidden(false);
-		items.get('Avviso_AttoUnicoOk').setHidden(false);
 		var dataAtto = values.get('AttoDiAdesioneUnico_dataProtocollo');
 		var numeroAtto = values.get('AttoDiAdesioneUnico_numeroProtocollo');
 		if ((dataAtto!== null) && (numeroAtto!== null) &&(dataAtto!== '') && (numeroAtto!== '')) {
@@ -93,21 +83,10 @@
 		    items.get('AttoDiAdesioneUnico_dataProtocollo_1').setHidden(true);
 		    items.get('AttoDiAdesioneUnico_numeroProtocollo_1').setHidden(true);
 		}
-		items.get('AttoDiAdesioneUnico_dataProtocollo_1').setHidden(false);
-		items.get('AttoDiAdesioneUnico_numeroProtocollo_1').setHidden(false);
-		// NASCONDI gli ITEM per la raccolta dati atto unico
-		fieldsets.get(idFildsetFirmatario).setHidden(true);
-		fieldsets.get(idFildsetDichiarazioni).setHidden(true);
-		
-		//TODO XXXXXXXXXXXXXx
-
-
-
 	} else {
 		isAttoUnicoPresentato = false;
 		// NASCONDI I DATI RELATIVI ALLA PROTOCOLLAZIONE dell'ATTO UNICO che non è stato ancora PRESENTATO
 		fieldsets.get(idFildsetDettagliAttoUnicoPresentato).setHidden(true);
-		items.get('Avviso_AttoUnicoOk').setHidden(true);
 		items.get('AttoDiAdesioneUnico_dataProtocollo_1').setHidden(true);
 		items.get('AttoDiAdesioneUnico_numeroProtocollo_1').setHidden(true);
 
@@ -115,13 +94,43 @@
 		values.put('AttoUnico_RappresentanteLegaleNome',values.get('Richiedente_RappresentanteLegaleNome'));
 		values.put('AttoUnico_RappresentanteLegaleCognome',values.get('Richiedente_RappresentanteLegaleCognome'));
 		values.put('AttoUnico_RappresentanteLegaleCodiceFiscale',values.get('Richiedente_RappresentanteLegaleCodiceFiscale'));
-
-		// MOSTRA gli ITEM per la raccolta dati atto unico
-		fieldsets.get(idFildsetFirmatario).setHidden(false);
-		fieldsets.get(idFildsetDichiarazioni).setHidden(false);
-		//TODO XXXXXXXXXXXXXx
-
 	}
+
+	// NASCONDI o MOSTRA fieldsets e ITEMS per la raccolta dati atto unico
+	fieldsets.get(idFildsetFirmatario).setHidden(isAttoUnicoPresentato);
+	fieldsets.get(idFildsetDichiarazioni).setHidden(isAttoUnicoPresentato);
+
+	// MOSTRA/NASCONDI AVVISO di atto unico ok e DATIPROCOCOLLAZIONE ATTO UNICO PRESENTATO
+	fieldsets.get(idFildsetDettagliAttoUnicoPresentato).setHidden(!isAttoUnicoPresentato);
+	items.get('Avviso_AttoUnicoOk').setHidden(!isAttoUnicoPresentato);
+
+	//Rende  non obbligatori / obbligatori ITEMS per la raccolta dati atto unico
+	items.get('AttoUnico_FirmatarioRappresentanteLegale').setRequired(!isAttoUnicoPresentato);
+	items.get('AttoUnico_RappresentanteLegaleNascitaProvincia').setRequired(!isAttoUnicoPresentato);
+	items.get('AttoUnico_RappresentanteLegaleNascitaComune').setRequired(!isAttoUnicoPresentato);
+	items.get('AttoUnico_RappresentanteLegaleNascitaData').setRequired(!isAttoUnicoPresentato);
+	items.get('AttoUnico_FirmatarioCodiceFiscale').setRequired(!isAttoUnicoPresentato);
+	items.get('AttoUnico_FirmatarioNome').setRequired(!isAttoUnicoPresentato);
+	items.get('AttoUnico_FirmatarioCognome').setRequired(!isAttoUnicoPresentato);
+	items.get('AttoUnico_FirmatarioNascitaProvincia').setRequired(!isAttoUnicoPresentato);
+	items.get('AttoUnico_FirmatarioNascitaComune').setRequired(!isAttoUnicoPresentato);
+	items.get('AttoUnico_FirmatarioNascitaData').setRequired(!isAttoUnicoPresentato);
+	items.get('SelezionaTutteDichiarazioni').setRequired(!isAttoUnicoPresentato);
+    var idItemDichiarazioni;
+	idItemDichiarazioni = items.get('Dichiaraz_0001'); if (idItemDichiarazioni !== null) {idItemDichiarazioni.setRequired(!isAttoUnicoPresentato);}
+	idItemDichiarazioni = items.get('Dichiaraz_0002'); if (idItemDichiarazioni !== null) {idItemDichiarazioni.setRequired(!isAttoUnicoPresentato);}
+	idItemDichiarazioni = items.get('Dichiaraz_0003'); if (idItemDichiarazioni !== null) {idItemDichiarazioni.setRequired(!isAttoUnicoPresentato);}
+	idItemDichiarazioni = items.get('Dichiaraz_0004'); if (idItemDichiarazioni !== null) {idItemDichiarazioni.setRequired(!isAttoUnicoPresentato);}
+	idItemDichiarazioni = items.get('Dichiaraz_0005'); if (idItemDichiarazioni !== null) {idItemDichiarazioni.setRequired(!isAttoUnicoPresentato);}
+	idItemDichiarazioni = items.get('Dichiaraz_0007'); if (idItemDichiarazioni !== null) {idItemDichiarazioni.setRequired(!isAttoUnicoPresentato);}
+	idItemDichiarazioni = items.get('Dichiaraz_0008'); if (idItemDichiarazioni !== null) {idItemDichiarazioni.setRequired(!isAttoUnicoPresentato);}
+	idItemDichiarazioni = items.get('Dichiaraz_0009'); if (idItemDichiarazioni !== null) {idItemDichiarazioni.setRequired(!isAttoUnicoPresentato);}
+	idItemDichiarazioni = items.get('Dichiaraz_0009_1'); if (idItemDichiarazioni !== null) {idItemDichiarazioni.setRequired(!isAttoUnicoPresentato);}
+	idItemDichiarazioni = items.get('Dichiaraz_0010'); if (idItemDichiarazioni !== null) {idItemDichiarazioni.setRequired(!isAttoUnicoPresentato);}
+	idItemDichiarazioni = items.get('Dichiaraz_0010_1'); if (idItemDichiarazioni !== null) {idItemDichiarazioni.setRequired(!isAttoUnicoPresentato);}
+	idItemDichiarazioni = items.get('Dichiaraz_0011'); if (idItemDichiarazioni !== null) {idItemDichiarazioni.setRequired(!isAttoUnicoPresentato);}
+	idItemDichiarazioni = items.get('Dichiaraz_0012'); if (idItemDichiarazioni !== null) {idItemDichiarazioni.setRequired(!isAttoUnicoPresentato);}
+	idItemDichiarazioni = items.get('Dichiaraz_0013'); if (idItemDichiarazioni !== null) {idItemDichiarazioni.setRequired(!isAttoUnicoPresentato);}
 
 
     // Verifica Accreditamento Operatore
@@ -166,6 +175,7 @@
     		"       and  inst.current_state in ('1629519c9d5f43d1bc8b6b036f4a4a8e','4a8eb456b84549a895bd0e87a59e0b67')                                   " +
 			"      )                                                                                                                                                                                                                                 " +
 			"";
+
 			var sqlLineaB=""+
 			"   select nvl(sum(to_number(DAT_VL, '999999999999999999999.9999999999999999999999999')),0) as IMPORTO_EROGATO " +
 			"     from ag_sm_data_entries data_entries                                                                                                                                                              " +
@@ -179,8 +189,10 @@
     		"       and  inst.current_state in ('1629519c9d5f43d1bc8b6b036f4a4a8e','4a8eb456b84549a895bd0e87a59e0b67')                                   " +
 			"      )                                                                                                                                                                                                                                 " +
 			"";
+
 				var ImportoErogatoLineaA = dizionarioService.getSingle(null, sqlLineaA);
 				var ImportoErogatoLineaB = dizionarioService.getSingle(null, sqlLineaB);
+
 				if (ImportoErogatoLineaA >= 3800000 ) {
 						items.get('Avviso_03').setHidden(false);
 				} else {
@@ -191,7 +203,9 @@
 				} else {
 						items.get('Avviso_04').setHidden(true);
 				}
+
 		}
+
 */
 
 
