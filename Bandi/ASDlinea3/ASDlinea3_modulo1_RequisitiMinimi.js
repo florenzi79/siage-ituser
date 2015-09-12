ac8de9fbeaa120b977ceeb7b26bea2704c329133
Francesco Lorenzi <francesco.lorenzi@gmail.com>
2015-09-12 16:37:58 +0200

    Aggiornamento ASD linea 3

--- Bandi/ASDlinea3/ASDlinea3_modulo1_RequisitiMinimi.js
+++ Bandi/ASDlinea3/ASDlinea3_modulo1_RequisitiMinimi.js
@@ -34,10 +34,10 @@ if (instance.getOwner() == user.getGruppoCorrente().getGroup().getId()) {
 values.put('title',values.get('SoggettoRichiedente_Denominazione') );
 items.get('title').setHidden(true);
 	//dati ASD
-var cfEccellenze = ["92008570134","01417790134","80143690156","13388220157","01766410169",
-										"05252350961","01892650126","96028090189","91003670154","91005500151",
-										"04932110960","02953290174","90006250170","92003320154","95035030121",
-										"03201460171","00444670194","01611770197","95007940133","80103250157"];
+var cfEccellenze = ["80143690156","13388220157","01766410169","05252350961","91005500151",
+										"04932110960","90006250170","95035030121","03201460171","00444670194",
+										"95007940133","96028090189","98028250177","01891620203","03240740120",
+										"92006910183","04498980152","91004580121","01790870164","13137360155"];
 var cfRichiedente = values.get('SoggettoRichiedente_CodiceFiscale');
 var i = 0;
 var cfPresente = false;
@@ -51,15 +51,7 @@ items.get('Avviso_SoggettoNonAmmissibile').setHidden(cfPresente);
 items.get('Richiedente_FsnDsaAppartenenza').setHidden(!cfPresente);
 items.get('Richiedente_Affiliazione').setHidden(!cfPresente);
 items.get('Richiedente_IscrizioneConiCip').setHidden(!cfPresente);
-if (cfRichiedente == '92008570134') {
-	values.put('Richiedente_FsnDsaAppartenenza','FEDERAZIONE ITALIANA DI ATLETICA LEGGERA');
-	values.put('Richiedente_Affiliazione','CO208');
-	values.put('Richiedente_IscrizioneConiCip','13494');
-} else if (cfRichiedente == '01417790134') {
-	values.put('Richiedente_FsnDsaAppartenenza','Federazione Italiana Pallacanestro');
-	values.put('Richiedente_Affiliazione','006237');
-	values.put('Richiedente_IscrizioneConiCip','14811');
-} else if (cfRichiedente == '80143690156') {
+if (cfRichiedente == '80143690156') {
 	values.put('Richiedente_FsnDsaAppartenenza','FEDERAZIONE ITALIANA DI ATLETICA LEGGERA');
 	values.put('Richiedente_Affiliazione','MI080');
 	values.put('Richiedente_IscrizioneConiCip','48344');
@@ -75,70 +67,78 @@ if (cfRichiedente == '92008570134') {
 	values.put('Richiedente_FsnDsaAppartenenza','FEDERAZIONE ITALIANA DI ATLETICA LEGGERA');
 	values.put('Richiedente_Affiliazione','MI089');
 	values.put('Richiedente_IscrizioneConiCip','17702');
-} else if (cfRichiedente == '01892650126') {
-	values.put('Richiedente_FsnDsaAppartenenza','FEDERAZIONE ITALIANA SCHERMA');
-	values.put('Richiedente_Affiliazione','18');
-	values.put('Richiedente_IscrizioneConiCip','9712');
-} else if (cfRichiedente == '96028090189') {
-	values.put('Richiedente_FsnDsaAppartenenza','FEDERAZIONE ITALIANA DI ATLETICA LEGGERA');
-	values.put('Richiedente_Affiliazione','PV110');
-	values.put('Richiedente_IscrizioneConiCip','49303');
-} else if (cfRichiedente == '91003670154') {
-	values.put('Richiedente_FsnDsaAppartenenza','Federazione Ginnastica d’Italia - Comitato Regionale Lombardia');
-	values.put('Richiedente_Affiliazione','000087');
-	values.put('Richiedente_IscrizioneConiCip','24223');
 } else if (cfRichiedente == '91005500151') {
-	values.put('Richiedente_FsnDsaAppartenenza','Federazione Ginnastica d’Italia - Comitato Regionale Lombardia');
+	values.put('Richiedente_FsnDsaAppartenenza','FEDERAZIONE GINNASTICA D’ITALIA');
 	values.put('Richiedente_Affiliazione','000610');
 	values.put('Richiedente_IscrizioneConiCip','687');
 } else if (cfRichiedente == '04932110960') {
-	values.put('Richiedente_FsnDsaAppartenenza','Federazione Ginnastica d’Italia - Comitato Regionale Lombardia');
+	values.put('Richiedente_FsnDsaAppartenenza','FEDERAZIONE GINNASTICA D’ITALIA');
 	values.put('Richiedente_Affiliazione','000046');
 	values.put('Richiedente_IscrizioneConiCip','1996');
-} else if (cfRichiedente == '02953290174') {
-	values.put('Richiedente_FsnDsaAppartenenza','FEDERAZIONE ITALIANA DI ATLETICA LEGGERA');
-	values.put('Richiedente_Affiliazione','BS181');
-	values.put('Richiedente_IscrizioneConiCip','13250');
 } else if (cfRichiedente == '90006250170') {
-	values.put('Richiedente_FsnDsaAppartenenza','FISDIR Lombardia');
+	values.put('Richiedente_FsnDsaAppartenenza','FISDIR LOMBARDIA');
 	values.put('Richiedente_Affiliazione','BS001');
 	values.put('Richiedente_IscrizioneConiCip','518');
-} else if (cfRichiedente == '92003320154') {
-	values.put('Richiedente_FsnDsaAppartenenza','Federazione Ginnastica d’Italia - Comitato Regionale Lombardia');
-	values.put('Richiedente_Affiliazione','000357');
-	values.put('Richiedente_IscrizioneConiCip','627');
 } else if (cfRichiedente == '95035030121') {
 	values.put('Richiedente_FsnDsaAppartenenza','FEDERAZIONE ITALIANA TIRO CON L’ARCO');
 	values.put('Richiedente_Affiliazione','04/006');
 	values.put('Richiedente_IscrizioneConiCip','27719');
 } else if (cfRichiedente == '03201460171') {
-	values.put('Richiedente_FsnDsaAppartenenza','Federazione Ginnastica d’Italia - Comitato Regionale Lombardia');
+	values.put('Richiedente_FsnDsaAppartenenza','FEDERAZIONE GINNASTICA D’ITALIA');
 	values.put('Richiedente_Affiliazione','000967');
 	values.put('Richiedente_IscrizioneConiCip','25172');
 } else if (cfRichiedente == '00444670194') {
 	values.put('Richiedente_FsnDsaAppartenenza','FEDERAZIONE ITALIANA PALLAVOLO');
 	values.put('Richiedente_Affiliazione','040150042');
 	values.put('Richiedente_IscrizioneConiCip','20825');
-} else if (cfRichiedente == '01611770197') {
-	values.put('Richiedente_FsnDsaAppartenenza','FISDIR Lombardia');
-	values.put('Richiedente_Affiliazione','cr004');
-	values.put('Richiedente_IscrizioneConiCip','374556');
 } else if (cfRichiedente == '95007940133') {
-	values.put('Richiedente_FsnDsaAppartenenza','FISDIR Lombardia');
+	values.put('Richiedente_FsnDsaAppartenenza','FISDIR LOMBARDIA');
 	values.put('Richiedente_Affiliazione','co001');
 	values.put('Richiedente_IscrizioneConiCip','542');
-} else if (cfRichiedente == '80103250157') {
-	values.put('Richiedente_FsnDsaAppartenenza','FEDERAZIONE ITALIANA SCHERMA');
-	values.put('Richiedente_Affiliazione','19');
-	values.put('Richiedente_IscrizioneConiCip','7692');
+} else if (cfRichiedente == '96028090189') {
+	values.put('Richiedente_FsnDsaAppartenenza','FEDERAZIONE ITALIANA DI ATLETICA LEGGERA');
+	values.put('Richiedente_Affiliazione','PV110');
+	values.put('Richiedente_IscrizioneConiCip','49303');
+} else if (cfRichiedente == '98028250177') {
+	values.put('Richiedente_FsnDsaAppartenenza','FEDERAZIONE ITALIANA NUOTO PARALIMPICO');
+	values.put('Richiedente_Affiliazione','BS006');
+	values.put('Richiedente_IscrizioneConiCip','180982');
+} else if (cfRichiedente == '01891620203') {
+	values.put('Richiedente_FsnDsaAppartenenza','FEDERAZIONE ITALIANA TENNISTAVOLO');
+	values.put('Richiedente_Affiliazione','2922');
+	values.put('Richiedente_IscrizioneConiCip','127037');
+} else if (cfRichiedente == '03240740120') {
+	values.put('Richiedente_FsnDsaAppartenenza','FIB COMITATO REGIONALE LOMBARDIA');
+	values.put('Richiedente_Affiliazione','RVA000168');
+	values.put('Richiedente_IscrizioneConiCip','64025');
+} else if (cfRichiedente == '92006910183') {
+	values.put('Richiedente_FsnDsaAppartenenza','FEDERAZIONE ITALIANA HOCKEY');
+	values.put('Richiedente_Affiliazione','030702');
+	values.put('Richiedente_IscrizioneConiCip','21729');
+} else if (cfRichiedente == '04498980152') {
+	values.put('Richiedente_FsnDsaAppartenenza','FEDERAZIONE ITALIANA SPORT PARALIMPICI E SPERIMENTALI');
+	values.put('Richiedente_Affiliazione','MI010');
+	values.put('Richiedente_IscrizioneConiCip','MI010');
+} else if (cfRichiedente == '91004580121') {
+	values.put('Richiedente_FsnDsaAppartenenza','FEDERAZIONE ITALIANA CANOA KAYAK');
+	values.put('Richiedente_Affiliazione','03020');
+	values.put('Richiedente_IscrizioneConiCip','8583');
+} else if (cfRichiedente == '01790870164') {
+	values.put('Richiedente_FsnDsaAppartenenza','FEDERAZIONE ITALIANA SPORT INVERNALI');
+	values.put('Richiedente_Affiliazione','BG33');
+	values.put('Richiedente_IscrizioneConiCip','33279');
+} else if (cfRichiedente == '13137360155') {
+	values.put('Richiedente_FsnDsaAppartenenza','FEDERAZIONE ITALIANA PALLAVOLO');
+	values.put('Richiedente_Affiliazione','04170191');
+	values.put('Richiedente_IscrizioneConiCip','1976');
 }
 
 //onChange
 //Validazione
-var cfEccellenze = ["92008570134","01417790134","80143690156","13388220157","01766410169",
-										"05252350961","01892650126","96028090189","91003670154","91005500151",
-										"04932110960","02953290174","90006250170","92003320154","95035030121",
-										"03201460171","00444670194","01611770197","95007940133","80103250157"];
+var cfEccellenze = ["80143690156","13388220157","01766410169","05252350961","91005500151",
+										"04932110960","90006250170","95035030121","03201460171","00444670194",
+										"95007940133","96028090189","98028250177","01891620203","03240740120",
+										"92006910183","04498980152","91004580121","01790870164","13137360155"];
 var cfRichiedente = values.get('SoggettoRichiedente_CodiceFiscale');
 var i = 0;
 var cfPresente = false;
