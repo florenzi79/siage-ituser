13b887f090b70b49b052c5119fa3365626407193
Fabiola Boldrini <fabiola.boldrini@elitedivision.it>
2015-09-23 16:14:16 +0200

    fabiola residenza lombardia o convitto

---
+++ Catalogo/Doti Costi Standard IFP/Validazione/3 - Residenza e domicilio(V).js
@@ -0,0 +1,34 @@
+{
+ var cap = values.get('Partecipante_ResidenzaCap');
+ if (cap !== null && cap !== '') {
+  var capInt = parseInt(cap);
+  if (!isValidCap(cap)) {
+   errors.put('Partecipante_ResidenzaCap', 'Partecipante_ResidenzaCap_val');
+  }
+ }
+}
+
+var email = getVal('Partecipante_Email');
+if (email !== null && email !== '') { 
+    if (!isValidEmail(email)) { 
+      err('Partecipante_Email', 'Partecipante_Email_val');
+    } 
+} 
+
+if (!(isProvinciaInRegioneIstat(values.get('Partecipante_ResidenzaProvincia'), '03'))) {
+    var residenza = false;
+	var idSede = values.get('Richiedente_IdSede_05');
+	var idOperatore = values.get('Richiedente_IdOperatore_05');
+	var comuneIstat = values.get('Partecipante_DomicilioComune');
+	    
+	if ((idSede == '292116' && idOperatore == '6185' && comuneIstat == '013242') ||
+		(idSede == '672769' && idOperatore == '120655' && comuneIstat == '013075') ||
+		(idSede == '303580' && idOperatore == '126372' && comuneIstat == '014060') ||
+		(idSede == '180414' && idOperatore == '171919' && comuneIstat == '017148') ||
+		(idSede == '662451' && idOperatore == '577313' && comuneIstat == '097015')){
+		    residenza = true;
+	}
+	if (residenza == false) {
+	    err('Partecipante_DomicilioComune','Partecipante_DomicilioComune_val');
+	}
+}
