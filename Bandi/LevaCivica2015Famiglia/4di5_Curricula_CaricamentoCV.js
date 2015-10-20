c5acad65eaddb6d3f708d2b84f4d57c7116f423c
Aldo Medri <medri@webguide.it>
2015-10-20 19:31:06 +0200

    Caricamento cv - con controllo evoluto sulle sedi

--- Bandi/LevaCivica2015Famiglia/4di5_Curricula_CaricamentoCV.js
+++ Bandi/LevaCivica2015Famiglia/4di5_Curricula_CaricamentoCV.js
@@ -1,23 +1,47 @@
 if (iamIn('DocumentiCurricula')){
     if (values.get(path+'RespAttivita_CognomeNome') === null) {
+		items.get('DocumentiCurricula.RespAttivita_CognomeNome').setReadonly(false);
+		items.get('DocumentiCurricula.CodiceIdentificativoSede').setReadonly(false);
         var nominativo;
         var codiceSede;
+        var codSede;
+        var sedeConCv;
         var pathIndex = parseInt(path.substr((path.lastIndexOf("[")+1), (path.lastIndexOf("]")-(path.lastIndexOf("[")+1))));
-        var i = 0;
-        var cv = 0;
+        var i;
+        var scv;
+        var dcv;
+        i = 0;
+        scv = 0;
         while (values.get('Sedi['+i+'].CvDisponibile') !== null) {
         	if (values.get('Sedi['+i+'].CvDisponibile') == 'false') {
-                if (pathIndex === cv) {
-                    nominativo = values.get('Sedi['+i+'].RespAttivita_CognomeNome');
-                    codiceSede = values.get('Sedi['+i+'].CodiceIdentificativoSede');
-                }
-        		cv++;
+				codSede = values.get('Sedi['+i+'].CodiceIdentificativoSede');
+				sedeConCv = false;
+				dcv = 0;
+				while (values.get('DocumentiCurricula['+dcv+'].CodiceIdentificativoSede') !== null) {
+					if (values.get('DocumentiCurricula['+dcv+'].CodiceIdentificativoSede') == codSede) {
+						sedeConCv = true;
+					}
+					dcv++;
+				}
+				if (sedeConCv === false) {
+					if (nominativo === undefined) {
+						codiceSede = codSede;
+						nominativo = values.get('Sedi['+i+'].RespAttivita_CognomeNome');
+					}
+				}
+        		scv++;
         	}
         	i++;
         }
         if (nominativo !== undefined) {
             values.put(path+'RespAttivita_CognomeNome',nominativo);
             values.put(path+'CodiceIdentificativoSede',codiceSede);
+			items.get('DocumentiCurricula.RespAttivita_CognomeNome').setReadonly(true);
+			items.get('DocumentiCurricula.CodiceIdentificativoSede').setReadonly(true);
         }
     }
+	else {
+		items.get('DocumentiCurricula.RespAttivita_CognomeNome').setReadonly(true);
+		items.get('DocumentiCurricula.CodiceIdentificativoSede').setReadonly(true);
+	}
 }
