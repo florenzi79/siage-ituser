e565965bf9e3d209c0c1d913f3153f2a1e69155f
Aldo Medri <medri@webguide.it>
2015-10-21 19:04:35 +0200

    I cv inseriti devono essere collegati alle sedi indicate

--- Bandi/LevaCivica2015Famiglia/4di5_Validazione.js
+++ Bandi/LevaCivica2015Famiglia/4di5_Validazione.js
@@ -1,16 +1,41 @@
-var i = 0;
-var cv = 0;
 var fcv = 0;
+var rcv = 0;
+var i = 0;
 while (values.get('Sedi['+i+'].CvDisponibile') !== null) {
 	if (values.get('Sedi['+i+'].CvDisponibile') == 'false') {
-		cv++;
+        rcv++;
 	}
 	i++;
 }
 while (values.get('DocumentiCurricula['+fcv+'].File_Curriculum') !== null) {
 	fcv++;
 }
-if (fcv != cv) {
+
+if (fcv == rcv) {
+    var erroreCvSedi = false;
+    var sedeConCv;
+    var scv = 0;
+    while (values.get('Sedi['+scv+'].CvDisponibile') !== null) {
+    	if (values.get('Sedi['+scv+'].CvDisponibile') == 'false') {
+            sedeConCv = false;
+            dcv = 0;
+            while (values.get('DocumentiCurricula['+dcv+'].CodiceIdentificativoSede') !== null) {
+    			if (values.get('DocumentiCurricula['+dcv+'].CodiceIdentificativoSede') == values.get('Sedi['+scv+'].CodiceIdentificativoSede')) {
+    				sedeConCv = true;
+    			}
+    			dcv++;
+        	}
+    		if (sedeConCv === false) {
+    		    erroreCvSedi = true;
+    		}
+            scv++;
+        }
+    }
+    if (erroreCvSedi) {
+    	errors.put('MessaggioMultiCv', 'MessaggioSediCv_val');
+    }
+}
+else {
 	errors.put('MessaggioMultiCv', 'MessaggioMultiCv_val');
-	println('WGX - fcv: ' + fcv + ' - cv: ' + cv + '.');
+	println('WGX - fcv: ' + fcv + ' - cv: ' + rcv + '.');
 }
